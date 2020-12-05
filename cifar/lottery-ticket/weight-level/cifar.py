eed668d133a724809073acb54ac50d7a511713a4
hugo-xie <xieyu9332@gmail.com>
2020-12-05 22:25:04 +0800

    test model

--- cifar/lottery-ticket/weight-level/cifar.py
+++ cifar/lottery-ticket/weight-level/cifar.py
@@ -118,6 +118,7 @@ def main():
     # Data
     print('==> Preparing dataset %s' % args.dataset)
     transform_train = transforms.Compose([
+        laplace_process,
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
