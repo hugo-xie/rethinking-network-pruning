4da0e5086e0a58189be20646cde63e4c5a3e0cd2
hugo-xie <xieyu9332@gmail.com>
2020-12-09 11:17:38 +0800

    test model

--- cifar/lottery-ticket/weight-level/cifar_rgb.py
+++ cifar/lottery-ticket/weight-level/cifar_rgb.py
@@ -160,7 +160,7 @@ def main():
     # Data
     print('==> Preparing dataset %s' % args.dataset)
     transform_train = transforms.Compose([
-        frequence_func,
+        #frequence_func,
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
@@ -168,7 +168,7 @@ def main():
     ])
 
     transform_test = transforms.Compose([
-        frequence_func,
+        #frequence_func,
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
